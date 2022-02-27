ff53f60be0adb1eaed52429de036d7a167c75a70
Swapneel Khandagale <53889874+Augustoandro@users.noreply.github.com>
2022-02-28 05:20:48 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,9 +4,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
  */
-define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'futureready');
+define('DB_SERVER', 'frt.database.windows.net');
+   define('DB_USERNAME', 'augusto');
+   define('DB_PASSWORD', 'pacman@1729');
+   define('DB_DATABASE', 'futurereadytalent');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
